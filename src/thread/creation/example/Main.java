ddbf4365ebfa1b0e11efc1f5734c89576c641de6
surajrocks1997 <surajrocks1997@gmail.com>
2020-07-16 17:54:10 +0530

    setting thread priority

--- src/thread/creation/example/Main.java
+++ src/thread/creation/example/Main.java
@@ -8,8 +8,12 @@ public class Main {
 			public void run() {
 				// TODO Auto-generated method stub
 				System.out.println("We are in new thread " + Thread.currentThread().getName());
+				System.out.println("Current thread priority " + Thread.currentThread().getPriority());
 			}
 		});
+		
+		thread.setName("New Worker Thread");
+		thread.setPriority(Thread.MAX_PRIORITY);
 
 		System.out.println("we are in thread: " + Thread.currentThread().getName() + " before starting a new thread");
 		thread.start();
